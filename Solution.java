4857eb035dafd745525e3a6088847b80d520aa0c
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-12-07 23:01:47 +0800

    day-7

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("../input.txt"); // change filepath as necessary
+
+        try (Scanner sc = new Scanner(file)) {
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+            }
+        }
+
+    }
+}
